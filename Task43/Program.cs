1347ff14f26acb68068ff00f396f69ff119cbe54
Natahis <natahis@bk.ru>
2022-08-27 11:56:27 +0300

    Task43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -3,4 +3,20 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите значения уравнений: ");
+Console.Write("b1 = ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k1 = ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b2 = ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k2 = ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+if (k1 == k2 && b1 != b2) Console.WriteLine("Прямые параллельны.");
+else if (k1 == k2 && b1 == b2) Console.WriteLine("Прямые совпадают.");
+else 
+{
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Координаты точки пересечения прямых -> ({x}; {y})");
+}
